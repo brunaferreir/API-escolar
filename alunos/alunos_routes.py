6f71010261c55f3033e7e92b5520f64cabd74bd7
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-03-04 15:59:30 -0300

    templates

--- alunos/alunos_routes.py
+++ alunos/alunos_routes.py
@@ -1,4 +1,4 @@
-# alunos_routes.py
+ #alunos_routes.py
 from flask import Blueprint, request, jsonify
 from .alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id, adicionar_aluno, atualizar_aluno, excluir_aluno
 
@@ -6,30 +6,31 @@ alunos_blueprint = Blueprint('alunos', __name__)
 
 @alunos_blueprint.route('/alunos', methods=['GET'])
 def get_alunos():
-    return jsonify(listar_alunos())
+     alunos = listar_alunos()
+     return jsonify(alunos)
 
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods=['GET'])
 def get_aluno(id_aluno):
-    try:
-        aluno = aluno_por_id(id_aluno)
-        return jsonify(aluno)
-    except AlunoNaoEncontrado:
-        return jsonify({'message': 'Aluno não encontrado'}), 404
+     try:
+         aluno = aluno_por_id(id_aluno)
+         return jsonify(aluno)
+     except AlunoNaoEncontrado:
+         return jsonify({'message': 'Aluno não encontrado'}), 404
 
 @alunos_blueprint.route('/alunos', methods=['POST'])
 def create_aluno():
-    data = request.json
-    adicionar_aluno(data)
-    return jsonify(data), 201
+     data = request.json
+     adicionar_aluno(data)
+     return jsonify(data), 201
 
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods=['PUT'])
 def update_aluno(id_aluno):
-    data = request.json
-    try:
-        atualizar_aluno(id_aluno, data)
-        return jsonify(aluno_por_id(id_aluno))
-    except AlunoNaoEncontrado:
-        return jsonify({'message': 'Aluno não encontrado'}), 404
+     data = request.json
+     try:
+         atualizar_aluno(id_aluno, data)
+         return jsonify(aluno_por_id(id_aluno))
+     except AlunoNaoEncontrado:
+         return jsonify({'message': 'Aluno não encontrado'}), 404
 
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods=['DELETE'])
 def delete_aluno(id_aluno):
@@ -37,4 +38,4 @@ def delete_aluno(id_aluno):
         excluir_aluno(id_aluno)
         return 'aluno deletado', 204
     except AlunoNaoEncontrado:
-        return jsonify({'message': 'Aluno não encontrado'}), 404
+         return jsonify({'message': 'Aluno não encontrado'}), 404
